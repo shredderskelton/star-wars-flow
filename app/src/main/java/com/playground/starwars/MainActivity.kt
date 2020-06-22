243113f2c9b7fc7f9f9f1b1da1188bb3e5ef600f
Nick Skelton <nick.g.skelton@gmail.com>
2020-06-22 16:57:40 +0200

    config changes bug

--- app/src/main/java/com/playground/starwars/MainActivity.kt
+++ app/src/main/java/com/playground/starwars/MainActivity.kt
@@ -18,7 +18,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.pushFragment(PeopleFragment())
+        if (savedInstanceState == null)
+            supportFragmentManager.pushFragment(PeopleFragment())
+
         supportFragmentManager.addOnBackStackChangedListener {
             if (supportFragmentManager.backStackEntryCount == 0) {
                 finish()
